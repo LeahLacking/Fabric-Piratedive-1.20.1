437f0b143afe2ea5f7de738d1347fe87ea820a19
Liam Middleton <liammiddleton@Liams-MacBook-Pro.local>
2023-09-27 17:20:31 +0900

    Initial commit

---
+++ src/main/java/net/leah/piratedive/PirateDiveClient.java
@@ -0,0 +1,10 @@
+package net.leah.piratedive;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PirateDiveClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/leah/piratedive/PirateDiveDataGenerator.java
@@ -0,0 +1,11 @@
+package net.leah.piratedive;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PirateDiveDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
